79607a1aa898a9733475a06cd27c1001caa20f2e
NeatCat7 <7mattewk@comcast.net>
2022-03-24 20:48:47 -0400

    Script update

--- src/kubejs/startup_scripts/script.js
+++ src/kubejs/startup_scripts/script.js
@@ -5,4 +5,6 @@ onEvent('item.registry', event => {
   // event.create('example_item').displayName('Example Item')
 
     event.create('mumble_alloy').displayName('Mumble Alloy')
+	event.create('coke_brick_blend').displayName('Coke Brick Blend')
+	event.create('coke_brick').displayName('Coke Brick')
 })
